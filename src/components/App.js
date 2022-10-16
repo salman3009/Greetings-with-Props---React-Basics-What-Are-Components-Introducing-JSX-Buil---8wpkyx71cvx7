b8fdde1a0be5c3e78965a753b7542a1ffb36f7aa
salman3009 <salman444g@gmail.com>
2022-10-16 10:37:38 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 import Welcome from './Welcome';
 
 const App = () => {
   return (
-    <Welcome name="" />
+    <Welcome name=""/>
   )
 }
 
